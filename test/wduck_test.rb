70a9730848dcfc82ee1b44d60ee7c681dfbfe999
Vysakh Sreenivasan <diplomatv@gmail.com>
2013-08-08 23:06:25 +0530

    Write sample test cases replace unit with minitest

---
+++ test/wduck_test.rb
@@ -0,0 +1,20 @@
+require_relative 'test_helper'
+
+describe WDuck::Duck do
+
+  subject { WDuck::Duck.new("linux") }
+  describe "function result that gets called for duck" do
+    it "should respond to the function call" do
+      subject.must_respond_to(:result)
+    end
+
+    it "function should be array of json objects" do
+      subject.result.must_be_instance_of(Array)
+    end
+  end
+
+  describe "check the results of for each condition" do
+  end
+
+
+end
