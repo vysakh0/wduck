0e62fb24afe9730cc8092ff7d41325136772fc05
Vysakh Sreenivasan <diplomatv@gmail.com>
2013-08-09 00:41:13 +0530

    Make changes in load path
    This load path shit is killing me! I could not figure out what the
    frigging shit is this that fuck! It works with "bundle exec" in dev but
    when installed the gem raises the freaking "load path error" WhY YOU NOT
    BEHAVE PROPERLY? :/

--- lib/wduck.rb
+++ lib/wduck.rb
@@ -1,6 +1,6 @@
+require 'wduck/version'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
 module Wduck
-  autoload :VERSION, 'wduck/version'
   autoload :Duck, 'wduck/duck'
 end
