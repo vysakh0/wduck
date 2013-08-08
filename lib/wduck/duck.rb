20c3e49907ea218dfe44d0abd7bc0cec0f93ed7c
Vysakh Sreenivasan <diplomatv@gmail.com>
2013-08-09 00:22:53 +0530

    Compete the gem, package it

--- lib/wduck/duck.rb
+++ lib/wduck/duck.rb
@@ -1,31 +1,33 @@
 module Wduck
-  require_relative 'duck_helper'
-  class Duck
+  module Duck
+    require_relative 'duck_helper'
+    class Ddg
 
-    include Wduck::DuckHelper
+      include Wduck::DuckHelper
 
-    def initialize(query)
-      @parsed_data = get_json_data(query)
-    end
+      def initialize(query)
+        @parsed_data = get_json_data(query)
+      end
 
-    def result
-      heading(@parsed_data)
-      source(@parsed_data)
+      def result
+        heading(@parsed_data)
+        source(@parsed_data)
 
-      if @parsed_data["Answer"].empty?
-        abstract_text(@parsed_data)
+        if @parsed_data["Answer"].empty?
+          abstract_text(@parsed_data)
 
-        if @parsed_data["AbstractText"].empty?
-          @parsed_data["RelatedTopics"].each_with_index do |result, index|
-            result_text(index, result)
+          if @parsed_data["AbstractText"].empty?
+            @parsed_data["RelatedTopics"].each_with_index do |result, index|
+              result_text(index, result)
+            end
           end
+
+        else
+          answer(@parsed_data)
         end
 
-      else
-        answer(@parsed_data)
       end
 
     end
-
   end
 end
